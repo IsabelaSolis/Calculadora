e500691a6cad4274953f5c5da3724170a1f558d5
Isabela Solis <a376827@uach.mx>
2026-02-05 14:37:55 -0600

    Second commit

--- src/main/java/com/example/demo/Calc1.java
+++ src/main/java/com/example/demo/Calc1.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Calc1 {
-  @GetMapping("/calc/{oper}/{n1}/{n2}")
+  @GetMapping("/calc/{oper}")
   public String calc(@PathVariable String oper,
-		  @PathVariable int n1,
-		  @PathVariable int n2) {
+		  @RequestParam (value = "n1", defaultValue = "0") int n1,
+		  @RequestParam (value = "n2", defaultValue = "0")int n2) {
 	  int res=0;
 	  if(oper.equals("suma")) {
 		  res=n1 + n2;
