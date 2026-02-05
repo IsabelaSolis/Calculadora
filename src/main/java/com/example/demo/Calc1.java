e5383f83e8d07398d8cb6439d4838e19f6898ccd
Isabela Solis <a376827@uach.mx>
2026-02-05 16:18:32 -0600

    Third commit, Final code whit the three annotations

--- src/main/java/com/example/demo/Calc1.java
+++ src/main/java/com/example/demo/Calc1.java
@@ -3,9 +3,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class Calc1 {
+	@PostMapping("/") 
+    public String createPerson(@RequestBody Person person) {
+        
+        System.out.println("Bienvenido  " + person.getName() + "a mi calculadora");
+
+        
+        return "Nombre recibido";
+    }
+	
+	
   @GetMapping("/calc/{oper}")
   public String calc(@PathVariable String oper,
 		  @RequestParam (value = "n1", defaultValue = "0") int n1,
@@ -15,9 +27,25 @@ public class Calc1 {
 		  res=n1 + n2;
 	  }else if (oper.equals("resta")) {
 		  res= n1 - n2;
-	  }else if (oper.equals("div")) {
+	  }else if (oper.equals("mul")) {
 		  res= n1* n2;
+	  }else if (oper.equals("div")) {
+		  res= n1 / n2;
 	  }
 	  return "Resultado=" + res;
   }
+}
+
+class Person{
+    private String name;
+
+    // Getters, setters, and constructors
+    public Person() {}
+
+    public Person(String name){
+        this.name = name;
+    }
+    public String getName(){
+    	return name; 
+    	}
 }
